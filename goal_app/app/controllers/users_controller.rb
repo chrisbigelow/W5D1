c9b136751c2699a027822a9ffb888acb932327b6
chris bigelow <chrisbigelow25@gmail.com>
2017-10-30 17:15:48 -0700

    Add Integration Tests

--- goal_app/app/controllers/users_controller.rb
+++ goal_app/app/controllers/users_controller.rb
@@ -5,7 +5,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to user_url(@user)
+      login(@user)
+      redirect_to users_url
     else
       flash[:errors] = @user.errors.full_messages
       render :new
